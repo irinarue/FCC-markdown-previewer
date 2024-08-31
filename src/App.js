bfe98671e9b12a7b6c53ddc620c324d0b0f95429
Irina Rüegg <rueegg.irina@gmail.com>
2024-08-31 10:49:32 +0200

    final commit aaand finitoo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import {marked} from 'marked';
+import './App.css'
+
+marked.setOptions({
+  breaks: true,
+});
 
 class App extends Component {
   constructor (props) {
     super(props) 
     this.state = {
-      input: "",
+      input: "# Welcome to the React Markdown Previewer!\n\n## You can adapt this editor to your liking and watch the changes in the previewer\n\n\n ### Here are some ideas:\n You can add some code inbetween backticks, like this: `<span></span>`\n\n```\n//or you can add multi-line code:\n\nfunction anExample (love) {\n if(love = true){\n  return 'she loves you';\n }\n}\n```\nYou can make text **bold**, _italic_, **_both_** or ~~cross stuff out~~\n\nIf you want you can add [links](https://kiriphuket.com/) of your favourite restaurant.\n >  And make Block Quotes!\n\n You can even add tables:\n\nWild Header | Crazy Header\n------------- | -------------\nYour content can be here |  and it can be here too.\nAnd here. | Okay.\n\n- You can also make lists.\n  - With different indentation.\n    - Maybe as bullets\n       - Or somthing else.\n\n1. You can also make numbered lists.\n1. But you always use the number one.\n1. Even if its the third point\n\nAnd finally you can also add images:\n![Happy Hacking](https://cdn.icon-icons.com/icons2/1468/PNG/512/hacker_101114.png)"
     }
     this.handleChange = this.handleChange.bind(this);
   };
@@ -24,14 +29,14 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>Markdown Previewer</h1>
+        <h1 id='mainTitle'>Markdown Previewer</h1>
         <div>
           <div className='half'>
-            <h3>Edit here:</h3>
+            <h3 className='mainSub'>Edit here:</h3>
             <textarea id='editor' value={this.state.input} onChange={this.handleChange}></textarea>
           </div>
           <div className='half'>
-            <h3>This is your preview:</h3>
+            <h3 className='mainSub'>This is your preview:</h3>
             <div id='preview' dangerouslySetInnerHTML={this.renderMarkdown(this.state.input)}></div>
           </div>
         </div>
