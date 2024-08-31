eafa0193d9982177776183a9afc4639a3580e837
Irina Rüegg <rueegg.irina@gmail.com>
2024-08-31 08:17:26 +0200

    Commit 2 alles aussert "marked und css"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import './App.css';
+/* global marked */
 import React, { Component } from 'react';
+
 
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
       input: "",
-      preview: "",
+     // preview: "",
     }
-
+    this.handleChange = this.handleChange.bind(this);
   };
+
+  handleChange (event) {
+    this.setState({
+      input: event.target.value,     
+    });
+  }
+
+  renderMarkdown (preview) {
+    return { __html: marked(preview)}; // Use the global marked variable
+  }
 
   render () {
     return (
       <div className="App">
-        
+        <h1>Markdown Previewer</h1>
+        <div>
+          <div className='half'>
+            <h3>Edit here:</h3>
+            <textarea id='editor' value={this.state.input} onChange={this.handleChange}></textarea>
+          </div>
+          <div className='half'>
+            <h3>This is your preview:</h3>
+            <div id='preview' dangerouslySetInnerHTML={this.renderMarkdown(this.state.preview)}></div>
+          </div>
+        </div>
       </div>
     );
   }
